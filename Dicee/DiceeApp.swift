35481dd7af48035405a071d0470d6aa83dee973e
Olivier Van hamme <nicefiction@gmail.com>
2021-04-02 09:41:14 +0200

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Olivier Van hamme on 02/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
